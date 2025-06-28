30fffab5562daa24621f73f6a6b12d1f4e5abeb6
atharvmahajan-idk <official.atharvmahajan8004@gmail.com>
2025-06-29 04:29:43 +0530

    getUserLastTenMess

--- src/utils/memoryFeature.utils.js
+++ src/utils/memoryFeature.utils.js
@@ -7,37 +7,62 @@ import fs from "fs/promises";
 import { randomUUID } from "crypto";
 import { userMap } from "../utils/maps.utils.js";
 
-async function memoryFeature(email, session, messLength = -3, redis = true) {
-    const { success, messagesList } = await getUserLastTenMess(
-        email,
-        messLength
+async function getUserLastTenMess(email, messLength = -4) {
+  try {
+    const messagesList = await RedisClient.lRange(
+      `users:${email}`,
+      messLength,
+      -1
     );
-    if(success){
-        let list = messagesList.map((obj) => {
-            try {
-              const parseObj = JSON.parse(obj);
-              // console.log(parseObj)
-              return parseObj.summaries;
-            } catch (error) {
-              memoryFeatureLogs(error.message);
-            }
-        });
-        const finalList = await GetEmbedings(list, email, session);
-        console.log(finalList);
-        let collectionName = "therapy-AI"
-        await client.upsert(collectionName, {
-            points: finalList, // each: { id, vector, payload }
-            wait: true
-        });
-       console.log("vector success fully upserted");
-       const user = userMap.get(email);
-       console.log("here is the users before and after");
-       console.log(user);
-       user.vectorPushes++;
-       console.log(user);
-       return true;
+    if (messagesList.length === 0) {
+      console.log("the list length is empty");
+      appendLog("list length is empty", "getUserLastTenMess");
+      return {
+        success: false,
+        messagesList,
+      };
+    } else if (messagesList.length > 0) {
+      console.log("list 200");
 
-
+      return {
+        success: true,
+        messagesList,
+      };
     }
+  } catch (error) {
+    console.log(error.message);
+    return {
+      success: false,
+    };
+  }
+}
+
+async function memoryFeature(email, session, messLength = -4, redis = true) {
+  const { success, messagesList } = await getUserLastTenMess(email, messLength);
+  if (success) {
+    let list = messagesList.map((obj) => {
+      try {
+        const parseObj = JSON.parse(obj);
+        // console.log(parseObj)
+        return parseObj.summaries;
+      } catch (error) {
+        memoryFeatureLogs(error.message);
+      }
+    });
+    const finalList = await GetEmbedings(list, email, session);
+    console.log(finalList);
+    let collectionName = "therapy-AI";
+    await client.upsert(collectionName, {
+      points: finalList, // each: { id, vector, payload }
+      wait: true,
+    });
+    console.log("vector success fully upserted");
+    const user = userMap.get(email);
+    console.log("here is the users before and after");
+    console.log(user);
+    user.vectorPushes++;
+    console.log(user);
+    return true;
+  }
 }
 export { memoryFeature };
