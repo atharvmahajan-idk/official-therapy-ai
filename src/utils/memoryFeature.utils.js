f441e870793103e67f9a42d60bcb1657beb80f94
atharvmahajan-idk <official.atharvmahajan8004@gmail.com>
2025-06-29 04:32:06 +0530

    GetEmbedings fnctions

--- src/utils/memoryFeature.utils.js
+++ src/utils/memoryFeature.utils.js
@@ -7,6 +7,78 @@ import fs from "fs/promises";
 import { randomUUID } from "crypto";
 import { userMap } from "../utils/maps.utils.js";
 
+async function GetEmbedings(messagesList, email, session) {
+    // memoryFeatureLogs(`\n${messagesList}`)
+    let list = messagesList.map((Obj) => {
+      console.log(Obj);
+      let obj = Obj;
+      console.log(obj)
+      let string = `
+              User Summary: ${obj.user_prompt}
+              AI Summary: ${obj.ai_response}
+              Mood Score: ${obj.moodScore}
+              Events: ${obj.events.length ? obj.events.join(", ") : "None"}
+              Emotional State: ${
+                obj.emotinol_state.length ? obj.emotinol_state.join(", ") : "None"
+              }
+          `.trim();
+      // return
+      return [
+        `User Summary: ${obj.user_prompt}`,
+        `AI Summary: ${obj.ai_response}`,
+        `Mood Score: ${obj.moodScore}`,
+        `Events: ${obj.events.length ? obj.events.join(", ") : "None"}`,
+        `Emotional State: ${
+          obj.emotinol_state.length ? obj.emotinol_state.join(", ") : "None"
+        }`,
+      ].join("\n"); // clean join without spaces or indentation
+    });
+    // console.log(list)
+    list.forEach((element) => {
+      console.log("\n", element);
+    });
+    const API_KEY = process.env.GEMINA_API_KEY;
+    // messagesList =  messagesList.map(obj=>{
+    //     let string = `
+    //         user_promptSummarie: ${obj.}
+    //     `
+    // })
+    try {
+      const ai = new GoogleGenAI({ apiKey: API_KEY });
+      const response = await ai.models.embedContent({
+        model: "gemini-embedding-exp-03-07",
+        contents: list,
+      });
+      let embeddings = response.embeddings;
+      const qdrantPoints = messagesList.map((obj, idx) => ({
+        id: randomUUID(),
+        vector: embeddings[idx].values, // from Gemini
+        payload: {
+          user_prompt: obj.user_prompt,
+          ai_response: obj.ai_response,
+          moodScore: obj.moodScore,
+          events: obj.events,
+          emotional_state: obj.emotinol_state,
+          email: email,
+          session: session,
+        },
+      }));
+  
+      return qdrantPoints;
+    } catch (error) {
+      const str = error.message;
+  
+      const match = str.match(/{.*}/s); // `s` flag allows dot to match newline characters too (if any)
+  
+      if (match) {
+        const jsonObj = JSON.parse(match[0]);
+        console.log(jsonObj);
+      } else {
+        console.log("No JSON object found.");
+      }
+    }
+}
+
 async function getUserLastTenMess(email, messLength = -4) {
   try {
     const messagesList = await RedisClient.lRange(
